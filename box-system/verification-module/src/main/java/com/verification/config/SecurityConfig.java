22e5c39551871663d81331b8184114f6717c270b
张禹 <yz8707346@gmail.com>
2025-04-25 16:15:02 +0800

    项目结构的改变

---
+++ box-system/verification-module/src/main/java/com/verification/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.verification.config;/**
+* @program: box-system
+*
+* @description: 
+*
+* @author: zhangxiaoyu
+*
+* @create: 2025-04-25 14:35
+**/
+public class SecurityConfig {
+}
---
+++ box-system/verification-module/src/main/java/com/verification/service/MyUserDetailsService.java
@@ -0,0 +1,11 @@
+package com.verification.service;/**
+* @program: box-system
+*
+* @description: 
+*
+* @author: zhangxiaoyu
+*
+* @create: 2025-04-25 14:37
+**/
+public class MyUserDetailsService {
+}
